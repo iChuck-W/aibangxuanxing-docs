6be2f20f68f0a42a3e13e4b05b8ea71c39b00d8b
iChuck-W <164290338+iChuck-W@users.noreply.github.com>
2025-07-24 18:38:42 +0800

    Update document path

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,15 @@ const config = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs/introduction',
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
