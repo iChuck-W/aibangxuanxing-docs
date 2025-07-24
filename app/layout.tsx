9d4b5c883abe40c0fcb18d208d8b59c500715017
iChuck-W <164290338+iChuck-W@users.noreply.github.com>
2025-07-24 20:11:22 +0800

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,32 @@ import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
+// 网站元数据配置，包括图标
+export const metadata: Metadata = {
+  title: {
+    template: '%s | 工业品智能专家',
+    default: '工业品智能专家',
+  },
+  description: '工业品智能专家以品牌官网为知识库做检索',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+  },
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="zh-CN" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
